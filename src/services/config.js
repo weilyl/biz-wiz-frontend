51c52dafea55b58d0491d13579d9cd0a0a480a8c
Weily Lang <weily.lang@gmail.com>
2021-04-21 15:04:45 -0400

    fixes token in localstorage issue

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
 export const apiURL = "https://biz-wiz.herokuapp.com/"; // "http://localhost:3030/"
 
-export const token = () => localStorage.getItem('token') || null;
+export const token = () => window.localStorage.getItem('token') || null;
 
