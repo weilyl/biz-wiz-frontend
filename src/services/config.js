475305d2a1aa9e724eec30a755d24fb3abe3283f
weilyl <64621097+weilyl@users.noreply.github.com>
2021-04-22 14:39:13 -0400

    Edits business post calls in services (#55)
    * adds register, login and logout using localStorage
    
    * refactors sign up form into functional component
    
    * implements custom hooks for register form handling
    
    * adds attempt at using React Bootstrap components for radio buttons
    
    * adds attempt at radio button using vanilla bootstrap
    
    * adds onclick to radio buttons
    
    * fixes home page redirect and other router errors
    
    * adds services and fixes business type bug in signup component
    
    * adds user name input field
    
    * adds user name input field
    
    * fixes token in localstorage issue
    
    * fixes localStorage for login and logout
    
    * fixes minor syntax error in navbar
    
    * merges map and search from organization dev
    
    * fixes controlled input bug
    
    * fixes dropdown menu select for business type
    
    * adds auth header to login and fixes logout
    
    * adds assign to register
    
    * redirects on login and logout
    
    * fixes react errors for value props and label tag for to htmlFor
    
    * fails attempt to successfully login at sign in page
    
    * debugs ternary check for signed in & successfully redirects upon log in of existing user
    
    * merges from organization dev
    
    * adds changes to posts services
    
    Co-authored-by: Weily Lang <weily.lang@gmail.com>

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
-export const apiURL = "https://biz-wiz.herokuapp.com/"; // "http://localhost:3030/"
+export const apiURL = "https://biz-wiz.herokuapp.com/"//"http://localhost:3030/"; 
 
 export let token = () => window.localStorage.getItem('token') || null;
 
