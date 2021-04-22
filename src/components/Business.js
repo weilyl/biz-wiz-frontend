4fd12de4d2e76cdc6a39efe40f37c7458f812c5a
Nila <Nila@LAPTOP-M0QERO78>
2021-04-21 20:58:30 -0400

    Added material-ui button to business home page

--- src/components/Business.js
+++ src/components/Business.js
@@ -5,13 +5,27 @@ import './Business.css'
 //import Post from "./Post.js";
 import CreatePost from "./Post.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  Typography,
+  Drawer,
+  Button,
+} from "@material-ui/core";
 
 function BusinessPage() {
   return (
     <div>
       <h1>Welcome to Your Page!</h1>
       <Router>
-        <Navbar bg="light" expand="lg">
+      <Nav.Link href="#home">
+        <Button><Link to="/CreatePost">Create Posts</Link></Button>
+      </Nav.Link>
+      <Nav.Link href="/">
+        <Button><Link to="/search">Search for Businesses</Link></Button>
+      </Nav.Link>
+        {/* <Navbar bg="light" expand="lg">
           <Navbar.Brand href="#home">Biz Wiz</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
@@ -24,7 +38,7 @@ function BusinessPage() {
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
-        </Navbar>
+        </Navbar> */}
         <Switch>
           <Route path="/CreatePost">
             <CreatePost />
